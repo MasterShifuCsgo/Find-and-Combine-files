0629131ad135d377f079c9ef83780a8af144263c
Kaspar Bergert <kasparbergert6@gmail.com>
2025-03-16 11:45:58 +0200

    Add project files.

---
+++ Find And Combine TXT files/Find And Combine TXT files.cpp
@@ -0,0 +1,81 @@
+
+#include <filesystem>
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <conio.h>  // for _getch(); ... press any key funciton
+// PURPOSE
+
+/*
+ Combine all the found txt files found going down from the partent directory and combine them into one txt file.
+
+*/
+namespace fs = std::filesystem;
+
+constexpr const char* Program_Output("root"); //folder
+constexpr const char* extension(".md");
+std::string fileName("merged");
+
+
+int main()
+{
+  //create the root path
+  fs::create_directory(Program_Output);
+  fs::path rootFolder{ fs::current_path() / Program_Output };
+
+  //merge.txt | append mode
+  std::ofstream output(rootFolder / fileName.append(extension), std::ios::app);
+
+  //directorys found
+  std::vector<fs::path> seenDirectories{fs::current_path()};
+  
+  //continues when there are no other directories to be seen
+  while (!seenDirectories.empty()) {
+    fs::path currentFolder = seenDirectories.back();
+    seenDirectories.pop_back();
+
+    //start looking for files in the directory
+    for (const auto& Entity : fs::directory_iterator(currentFolder)) {
+    
+      if (Entity.path().filename() == '\"'+fileName + extension+'\"') {
+        break;
+      }
+
+      //cut out symlink files
+      if (fs::is_regular_file(Entity.path())) {
+        
+        //if current file is a txt file, append the contents to the merged.txt file
+        if (Entity.path().extension() == extension) {
+          
+          //open the found .txt file
+          std::ifstream target (Entity.path());          
+
+          //append the text from the file into the main text file
+          output << Entity.path().filename() << '\n';          
+          std::string line;
+          while (std::getline(target, line)) {
+            output << line;
+          }                
+
+          output << "\n\n\n";
+          target.close();
+        }
+        
+      }     
+      
+      //see if the entity is a directory and if directory is not root directory
+      if (fs::is_directory(Entity.path()) && Entity.path() != Program_Output) {
+        seenDirectories.push_back(Entity.path());
+        
+      }
+
+    }
+  }
+
+  
+  _getch();
+
+  return 0;
+}
+
