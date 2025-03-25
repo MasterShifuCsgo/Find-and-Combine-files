b0c505086d787857f896a61327423a7b6e25c485
Kaspar Bergert <kasparbergert6@gmail.com>
2025-03-25 22:26:30 +0200

    fixed template defenition

--- Find And Combine TXT files/GlobalFunctions.cpp
+++ Find And Combine TXT files/GlobalFunctions.cpp
@@ -5,30 +5,4 @@
 #include <string>
 
 
-template<typename T>
-bool GlobalFunctions::readUserInput(T& user_choice, const std::string message_when_failed) {
-  std::cin >> user_choice;
 
-  if (std::cin.fail()) {
-    std::cin.clear(); // Reset error state
-    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); // Discard invalid input
-
-    std::cout << "\nInvalid Choice!\n";
-    std::cout << message_when_failed << '\n';
-    return false;
-  }
-
-  return true;
-}
-
-template<typename T>
-bool GlobalFunctions::isValidChoice(const T& user_choice, const std::vector<T> valid_choices, const std::string message_when_failed) {
-  for (const T& choice : valid_choices) {
-    if (user_choice == choice) {
-      return true;
-    }
-  }
-
-  std::cout << message_when_failed << '\n';
-  return false;
-}
--- Find And Combine TXT files/GlobalFunctions.h
+++ Find And Combine TXT files/GlobalFunctions.h
@@ -10,12 +10,32 @@ namespace GlobalFunctions {
   // prints `message_when_failed`, clears the error, and returns false.
   // Otherwise, returns true.
   template<typename T>
-  extern bool readUserInput(T& user_choice, const std::string message_when_failed);
+  extern bool readUserInput(T& user_choice, const std::string message_when_failed) {
+    std::cin >> user_choice;
+    if (std::cin.fail()) {
+      std::cin.clear(); // Reset error state
+      std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); // Discard invalid input
+      std::cout << "\nInvalid Choice!\n";
+      std::cout << message_when_failed << '\n';
+      return false;
+    }
+
+    return true;
+  }
 
   // Validates if `user_choice` is in the `valid_choices` list.
   // If not, prints `message_when_failed` and returns false.
   // Otherwise, returns true.
   template<typename T>
-  extern bool isValidChoice(const T& user_choice, const std::vector<T> valid_choices, const std::string message_when_failed);
+  extern bool isValidChoice(const T& user_choice, const std::vector<T> valid_choices, const std::string message_when_failed) {
+    for (const T& choice : valid_choices) {
+      if (user_choice == choice) {
+        return true;
+      }
+    }
+
+    std::cout << message_when_failed << '\n';
+    return false;
+  };
  
 }
