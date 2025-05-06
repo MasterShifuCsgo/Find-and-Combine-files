31e958cc2bcee08171ad2ebdec08448561edd139
MasterShifu <136521147+MasterShifuCsgo@users.noreply.github.com>
2025-05-06 21:59:49 +0300

    Update Find And Combine TXT files.cpp

--- Find And Combine TXT files/Find And Combine TXT files.cpp
+++ Find And Combine TXT files/Find And Combine TXT files.cpp
@@ -1,30 +1,16 @@
 #include <iostream>
 #include <thread>
 #include <chrono>
+#include "Traverser.h" // Handles file traversal + combination
 
-#include "Traverser.h" 
+int main() {
+  if (Traverser().combine())
+    std::cout << "\n\nFinished\n\n";
 
-// PURPOSE
-//Combine all the found txt files found going down from the partent directory.
-
-// The program will search for all the files in the current directory and all subdirectories.
-int main()
-{
-  
-  Traverser tr;
-  if (tr.combine()) {
-    std::cout << "\n\nFinished\n\n" << std::endl;
-  }
-
-  int seconds = 3;
-
-  for (int i = seconds; i > 0; i--) {
-    std::cout << "exiting in " << i << "seconds\n";
+  for (int i = 3; i > 0; --i) {
+    std::cout << "Exiting in " << i << " seconds\n";
     std::this_thread::sleep_for(std::chrono::seconds(1));
   }
-  
-  
 
   return 0;
 }
-
